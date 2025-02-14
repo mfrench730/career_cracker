daf3e6c642f88d8596457612bd38df17741707a5
Mohammed Alshmayri <mohamedalshmayri@gmail.com>
2025-02-13 22:52:16 -0500

    Update settings.py

--- backend/careercracker/settings.py
+++ backend/careercracker/settings.py
@@ -82,8 +82,11 @@ REST_FRAMEWORK = {
     ),
 }
 
+CORS_ALLOW_CREDENTIALS = True
+
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5432",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
 ]
 
 # Database
