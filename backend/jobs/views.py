6d622116d472a5c094886409f638f90871841de3
mfrench730 <mfrench730@gmail.com>
2025-03-25 17:07:53 -0400

    career info view update

--- backend/jobs/views.py
+++ backend/jobs/views.py
@@ -1,26 +1,51 @@
 from django.shortcuts import render
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .utils import api_client
 from .utils.OnetWebService import OnetWebService
+from rest_framework.permissions import IsAuthenticated
 import os
 
-# example call for this: GET /api/tasks/?job_title=Software%20Engineer
-@api_view(['GET'])
-def get_info(request):
-    # job_title parameter is pulled from frontend POST
-    job_title = request.GET.get('job_title') # extract from query params
-    if not job_title:
-        return Response({'error': 'Job title is required'}, status=400)
-    
-    onet_ws = OnetWebService(os.environ.get("ONET_USERNAME"), os.environ.get("ONET_PASSWORD"))
-    
-    description = api_client.get_job_info(api_client.get_soc_code(job_title, onet_ws), onet_ws)
-    tasks = api_client.get_tasks(api_client.get_soc_code(job_title, onet_ws), onet_ws)
 
-    return Response({'description': description,
-                     'tasks': tasks
-                     })
+class get_info_view(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        job_title = request.GET.get('job_title')
+        if not job_title:
+            return Response({'error': 'Job title is required.'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        # O*NET instance
+        onet_ws = OnetWebService(os.environ.get("ONET_USERNAME"), os.environ.get("ONET_PASSWORD"))
+
+        description = api_client.get_job_info(api_client.get_soc_code(job_title, onet_ws), onet_ws)
+        tasks = api_client.get_tasks(api_client.get_soc_code(job_title, onet_ws), onet_ws)
+
+        data = {
+            'description': description,
+            'tasks': tasks
+        }
+        return Response(data)
+
+
+
+# example call for this: GET /api/tasks/?job_title=Software%20Engineer
+# @api_view(['GET'])
+# def get_info(request):
+#     # job_title parameter is pulled from frontend POST
+#     job_title = request.GET.get('job_title') # extract from query params
+#     if not job_title:
+#         return Response({'error': 'Job title is required'}, status=400)
+    
+#     onet_ws = OnetWebService(os.environ.get("ONET_USERNAME"), os.environ.get("ONET_PASSWORD"))
+    
+#     description = api_client.get_job_info(api_client.get_soc_code(job_title, onet_ws), onet_ws)
+#     tasks = api_client.get_tasks(api_client.get_soc_code(job_title, onet_ws), onet_ws)
+
+#     return Response({'description': description,
+#                      'tasks': tasks
+#                      })
 
 
